87d74b240e8ec5a824010f1172561668f9cfd808
tuyishimekyrie <tuyishimekyrie@gmail.com>
2026-06-13 21:59:00 +0300

    fix: change file endpoint from sync to async

--- app/api/v1/file/file.py
+++ app/api/v1/file/file.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, Depends, HTTPException, Response, status
-from sqlalchemy.orm import Session
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.db.dependencies import get_db_session
 from app.service.document.document import get_file_by_id
@@ -8,7 +8,7 @@ router = APIRouter(prefix="/file", tags=["file"])
 
 
 @router.get("/{id}")
-async def get_file_endpoint(id: int, db_session: Session = Depends(get_db_session)):
+async def get_file_endpoint(id: int, db_session: AsyncSession = Depends(get_db_session)):
     result = await get_file_by_id(id=id, db_session=db_session)
 
     if result:

d6ab66db13e92ca8e865db442d6c539e19bc7cbe
tuyishimekyrie <tuyishimekyrie@gmail.com>
2026-06-21 23:53:15 +0300

    refact: structure the response

--- app/api/v1/document/document.py
+++ app/api/v1/document/document.py
@@ -61,7 +61,7 @@ async def get_document_endpoint(id: int, db_session: AsyncSession = Depends(get_
 @router.get("?")
 async def get_documents_endpoint(page: int = Query(default=1, title="Current page", description="The current page to display items"), limit: int = Query(default=10, title="limit", description="limit of items per page", gt=1, le=50), db_session: AsyncSession = Depends(get_db_session)):
     result, total_documents =  await get_documents(page=page, limit=limit, db_session=db_session)
-    return result, total_documents
+    return {"data": result, "total_documents": total_documents}
 
 
 @router.delete("/document/{id}")
